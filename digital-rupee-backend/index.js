8b2f95f531e7a356c75363cc6cc8d2404b24a9c0
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2023-07-23 22:41:04 +0530

    Registartion Form Created

--- digital-rupee-backend/index.js
+++ digital-rupee-backend/index.js
@@ -3,7 +3,8 @@ const cors=require('cors');
 const app = express();
 const port = 4000; // You can change this to any port number you prefer
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 // Define a route
 app.get('/', (req, res) => {
@@ -14,6 +15,18 @@ app.get('/backendApi/helloWorld', (req, res) => {
     res.send('Hello World!');
 });
 
+// Define the POST route for registration
+app.post('/backendApi/register', (req, res) => {
+    // Extract the registration data from the request body
+    const { name, email, password, balance } = req.body;
+  
+    // You can now handle the registration data, e.g., save it to a database
+    console.log('Received registration data:');
+    console.log({ name, email, password, balance });
+  
+    // Send a response back to the client
+    res.status(200).json({ message: 'Registration successful!' });
+  });
 
 // Start the server
 app.listen(port, () => {
