57a5caa9687415003576f0ca248f709272e82ea8
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2023-07-23 21:32:46 +0530

    Router link created

--- digital-rupee-frontend/src/App.js
+++ digital-rupee-frontend/src/App.js
@@ -2,16 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './allComponents/Header';
 import HelloWorld from './allComponents/HelloWorld';
+import Home from './allComponents/Home';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-
-    <>
-    <Header/>
-    <HelloWorld/>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/HelloWorld" element={<HelloWorld />} />
+      </Routes>
+    </Router>
 
   );
 }
+
 
 export default App;
